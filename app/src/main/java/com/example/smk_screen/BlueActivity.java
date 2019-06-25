a419bdaa727408a52eead89794de342f1541cf62
zloi666 <linnik.sergey@gmail.com>
2019-06-26 00:53:36 +0300

    + real fullscreen - small bugs

--- app/src/main/java/com/example/smk_screen/BlueActivity.java
+++ app/src/main/java/com/example/smk_screen/BlueActivity.java
@@ -1,6 +1,8 @@
 package com.example.smk_screen;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,10 @@ public class BlueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_green);
+        setContentView(R.layout.activity_blue);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
     }
 }
--- app/src/main/java/com/example/smk_screen/GreenActivity.java
+++ app/src/main/java/com/example/smk_screen/GreenActivity.java
@@ -2,6 +2,8 @@ package com.example.smk_screen;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,8 @@ public class GreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_green);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
     }
 }
